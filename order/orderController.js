f82f08102a2e895f2459a9f49eeb2eb57b563a24
JDK <ehdrjf2062@gmail.com>
2022-09-11 22:55:59 +0900

    feat: 주문내역 컨트롤러 작성

---
+++ order/orderController.js
@@ -0,0 +1,44 @@
+const orderService = require("./orderService");
+const { StatusCodes } = require("http-status-codes");
+
+async function createOrder(req, res, next) {
+  const order = req.body;
+  try {
+    await orderService.createOrder(order);
+    return res.status(StatusCodes.OK).send({ message: "CREATED" });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getOrder(req, res, next) {
+  const id = req.params.id;
+  try {
+    const order = await orderService.getOrder(id);
+    return res.status(StatusCodes.OK).send({ message: "OK", data: order });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateOrder(req, res, next) {
+  const order = req.body;
+  try {
+    await orderService.updateOrder(order);
+    return res.status(StatusCodes.OK).send({ message: "UPDATED" });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function deleteOrder(req, res, next) {
+  const id = req.params.id;
+  try {
+    await orderService.deleteOrder(id);
+    return res.status(StatusCodes.OK).send({ message: "DELETED" });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { createOrder, getOrder, updateOrder, deleteOrder };
