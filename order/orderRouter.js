32ceea38db6ad78cdc0e62582dc792984e3ce088
JDK <ehdrjf2062@gmail.com>
2022-09-12 01:31:14 +0900

    chore: TODO주석 작성

--- order/orderRouter.js
+++ order/orderRouter.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const orderController = require("./orderController");
 
+//TODO: 유효성 검사
+
 router.post("/", orderController.createOrder);
 router.get("/:id", orderController.getOrder);
 router.put("/", orderController.updateOrder);
